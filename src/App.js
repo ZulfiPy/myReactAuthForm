3e87a0364bf0cd6b60f7081369b705a52031585d
ZulfiPy <zulfizupy@gmail.com>
2024-02-03 10:34:09 +0200

    React router configured in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
 import Register from "./components/Register";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Layout from "./components/Layout";
+import Editor from "./components/Editor";
+import Admin from "./components/Admin";
+import Missing from "./components/Missing";
+import Unauthorized from "./components/Unauthorized";
+import Lounge from "./components/Lounge";
+import Linkpage from "./components/LinkPage";
+import RequireAuth from "./components/RequireAuth";
+import { Routes, Route } from "react-router-dom";
+
+const ROLES = {
+  'User': process.env.REACT_APP_USER,
+  'Editor': process.env.REACT_APP_EDITOR,
+  'Admin': process.env.REACT_APP_ADMIN
+};
 
 function App() {
   return (
-    <main className="App">
-      <Register />
-    </main>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="linkpage" element={<Linkpage />} />
+          <Route path="unauthorized" element={<Unauthorized />} />
+
+          <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+
+          <Route element={<RequireAuth allowedRoles={[ROLES.Editor]} />}>
+            <Route path="editor" element={<Editor />} />
+          </Route>
+
+          <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
+            <Route path="admin" element={<Admin />} />
+          </Route>
+
+          <Route element={<RequireAuth allowedRoles={[ROLES.Editor, ROLES.Admin]} />}>
+            <Route path="lounge" element={<Lounge />} />
+          </Route>
+
+          <Route path="*" element={<Missing />} />
+        </Route>
+      </Routes>
   );
 }
 
