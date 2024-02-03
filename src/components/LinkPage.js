c3486524830e0a0d99c5a9290e83cfcb949c5042
ZulfiPy <zulfizupy@gmail.com>
2024-02-03 06:46:19 +0200

    link page created

---
+++ src/components/LinkPage.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+const Linkpage = () => {
+    return (
+        <section>
+            <h1>Links</h1>
+            <br />
+            <h2>Public</h2>
+            <br />
+            <Link to="login">Login</Link>
+            <Link to="/register">Register</Link>
+            <br />
+            <Link to="/">Home</Link>
+            <Link to="/editor">Editors Page</Link>
+            <Link to="/admin">Admin Page</Link>
+        </section>
+    )
+}
+
+export default Linkpage;
