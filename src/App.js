925fde92176713780d68b01909abc3611a08c880
ZulfiPy <zulfizupy@gmail.com>
2024-02-03 06:35:49 +0200

    Revert "a tag replaced by Link from react-router-dom"
    This reverts commit 52c6f1c63b0f23ea2e14efbc5dd49fdf6fb103eb.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import Register from "./components/Register";
-import Login from "./components/Login";
 
 function App() {
   return (
     <main className="App">
-      <Login />
+      <Register />
     </main>
   );
 }
