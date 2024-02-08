34155243629127b738b983d020593c405499b839
ZulfiPy <zulfizupy@gmail.com>
2024-02-08 06:39:12 +0200

    setAuth upd.

--- src/hooks/useRefreshToken.js
+++ src/hooks/useRefreshToken.js
@@ -7,19 +7,25 @@ const useRefreshToken = () => {
     // Defining an asynchronous function to refresh the access token.
     const refresh = async () => {
         // Making a GET request using SIMPLE axios to get a new access token
-        const response = await axios.get('/refresh', {
-            withCredentials: true
-        });
-
-        // Updating the authentication state with the new access token 
-        setAuth(prev => {
-            console.log('prev data', JSON.stringify(prev));
-            console.log('response.data.accessToken', response.data.accessToken);
-            // update auth state by returning updated authentication state with the new access token
-            return { ...prev, accessToken: response.data.accessToken };
-        });
-        // return the new access token
-        return response.data.accessToken;
+        try {
+            const response = await axios.get('/refresh', {
+                withCredentials: true
+            });
+            // Updating the authentication state with the new access token 
+            setAuth(prev => {
+                console.log('prev data', JSON.stringify(prev));
+                // update auth state by returning updated authentication state with the new access token
+                return {
+                    ...prev,
+                    roles: response.data?.roles,
+                    accessToken: response.data.accessToken
+                };
+            });
+            // return the new access token
+            return response.data.accessToken;
+        } catch (err) {
+            console.log(err)
+        }
     }
     // return the refresh function itself
     return refresh;
