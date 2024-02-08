7c4ad53fc7b1679c77699531502fc317e4cce211
ZulfiPy <zulfizupy@gmail.com>
2024-02-08 06:37:31 +0200

    disable react dev tools if in prod

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { AuthProvider } from "./context/AuthProvider";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
+
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
@@ -15,5 +19,6 @@ root.render(
         </Routes>
       </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+  document.getElementById('root')
+);  
