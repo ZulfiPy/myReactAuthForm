69938f28bf80cd44a49a18400b1ab7100dc53a42
ZulfiPy <zulfizupy@gmail.com>
2024-02-08 06:50:09 +0200

    user username rendering cleared out

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,15 +1,12 @@
 import { Link } from "react-router-dom";
-import useAuth from "../hooks/useAuth";
 import Drivers from "./Drivers";
 
 const Admin = () => {
-    const { auth } = useAuth();
 
     return (
         <section>
             <h1>Admin Page</h1>
             <br />
-            <p>Welcome, to the Admin page {auth.username ? auth.username : "undefined user"}!</p>
             <Drivers />
             <br />
             <Link to="/">Home</Link>
